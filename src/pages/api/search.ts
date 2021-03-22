25e1549399d0ae354f921901ada029a8656257a0
SMCodesP <samuel.silva513@etec.sp.gov.br>
2021-03-22 20:16:05 +0000

    Add cache in request search

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -2,7 +2,10 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import api from '../../services/api'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  res.setHeader('Cache-Control', 's-maxage=604800')
+
   const { query } = req.query
+
   const animes = await api.directSearchAnime(String(query))
 
   if (!animes) return res.json([])
