3a57a021ce39a38c6b3b0f3c46af942408341452
SMCodesP <samuel.silva513@etec.sp.gov.br>
2021-05-25 09:25:09 -0300

    Use image anilist in anime resume

--- src/components/AnimeResume/index.tsx
+++ src/components/AnimeResume/index.tsx
@@ -19,46 +19,51 @@ const AnimeResume: React.FC<{
   const [session, loading] = useSession()
   const [hoverFavorite, setHoverFavorite] = useState(false)
 
-  const MarkFavorite = () => (isFavorite(anime.id) || hoverFavorite) ? (
-    <FaBookmark
-      size={24}
-      color={theme.text}
-      style={{
-        alignSelf: 'flex-end',
-        marginTop: 8,
-        marginRight: 8,
-        position: 'relative',
-        zIndex: 9999,
-      }}
-      onClick={() => toggleFavorite(anime.id)}
-      onMouseLeave={() => setHoverFavorite(false)}
-    />
-  ) : (
-    <FaRegBookmark
-      size={24}
-      color={theme.text}
-      style={{
-        alignSelf: 'flex-end',
-        marginTop: 8,
-        marginRight: 8,
-        position: 'relative',
-        zIndex: 9999,
-      }}
-      onClick={() => toggleFavorite(anime.id)}
-      onMouseEnter={() => setHoverFavorite(true)}
-    />
-  )
+  const MarkFavorite = () =>
+    isFavorite(anime.id) || hoverFavorite ? (
+      <FaBookmark
+        size={24}
+        color={theme.text}
+        style={{
+          alignSelf: 'flex-end',
+          marginTop: 8,
+          marginRight: 8,
+          position: 'relative',
+          zIndex: 9999,
+        }}
+        onClick={() => toggleFavorite(anime.id)}
+        onMouseLeave={() => setHoverFavorite(false)}
+      />
+    ) : (
+      <FaRegBookmark
+        size={24}
+        color={theme.text}
+        style={{
+          alignSelf: 'flex-end',
+          marginTop: 8,
+          marginRight: 8,
+          position: 'relative',
+          zIndex: 9999,
+        }}
+        onClick={() => toggleFavorite(anime.id)}
+        onMouseEnter={() => setHoverFavorite(true)}
+      />
+    )
 
   return (
     <ContainerAnime>
       <Link href={`/anime/${anime.id}`}>
         <a>
           <Image
-            src={`https://cdn.appanimeplus.tk/img/${anime.category_image}`}
+            src={
+              anime.anilist
+                ? anime.anilist?.coverImage.extraLarge
+                : `https://cdn.appanimeplus.tk/img/${anime.category_image}`
+            }
           />
         </a>
       </Link>
-      {(session && !loading) && <MarkFavorite />}
+      {session && !loading && <MarkFavorite />}
       <More
         style={{
           padding: '5px 15px',
