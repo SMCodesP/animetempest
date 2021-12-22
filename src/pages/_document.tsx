7a69111af8f182aab97e9595844470837fee5e91
SMCodesP <samuel.silva513@etec.sp.gov.br>
2021-12-22 15:28:35 -0300

    fix: remove error in preconnect crossorigin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,7 +39,7 @@ export default class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Rubik:ital,wght@0,400;0,500;0,700;1,700&display=swap"
             rel="stylesheet"
